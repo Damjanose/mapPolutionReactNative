437e079abec1a6a65a0583ecc70025e1a7e8513a
IARNEDNIA\Damjano <damjanoda@gmail.com>
2023-05-24 18:04:56 +0200

    chore: fixed style in homePage

--- app/(tabs)/index.js
+++ app/(tabs)/index.js
@@ -1,19 +1,18 @@
-import { StyleSheet, Text, View, ImageBackground } from "react-native";
-import { Link } from 'expo-router'
+import {StyleSheet, Text, View, ImageBackground, TouchableOpacity } from "react-native";
+import { useRouter } from 'expo-router'
 
 export default function Page() {
   const image = {uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRO3mgiYtaMFWjsvNEpTbVrj4UQQ2auMZtc_A&usqp=CAUs'};
+  const { push } = useRouter()
 
   return (
     <View style={styles.container}>
-      <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+      <ImageBackground source={image} resizeMode="cover" style={styles.image} blurRadius={0.7}>
         <Text style={[styles.title, styles.colorWhite]}>Pollution map</Text>
         <Text style={[styles.subtitle, styles.colorWhite]}>As high level of pollution we decided to build an application to let people know about some of the most polluted places in Tirana. </Text>
-        <View style={[styles.padTop20]}>
-          <Text style={styles.colorWhite}>So if you are interested</Text>
-          <Link href="/map" style={[styles.link, styles.colorWhite]}> Click here </Link>
-          <Text style={styles.colorWhite}>and you will be redirected to a map</Text>
-        </View>
+        <TouchableOpacity onPress={()=> push('/map')} style={styles.button}>
+          <Text style={styles.colorWhite}>Go to Map</Text>
+        </TouchableOpacity>
       </ImageBackground>
     </View>
   );
@@ -21,29 +20,26 @@ export default function Page() {
 
 const styles = StyleSheet.create({
   container: { flex: 1 },
-  main: {
-    flex: 1,
-    justifyContent: "center",
-  },
-  image: {
-    flex: 1,
-    justifyContent: 'center',
-    padding: 25,
-  },
+  main: { flex: 1, justifyContent: "center" },
+  image: { flex: 1, justifyContent: 'center', padding: 25 },
   title: {
-    fontSize: 30,
+    fontSize: 37,
     fontWeight: "bold",
     lineHeight: 60,
+    position: "relative",
+    bottom: 80,
+    textAlign: "center"
   },
-  subtitle: {
-    fontSize: 20,
-    color: "#38434D",
-  },
-  link: {
-    color: '#a0b9db',
-    textDecorationLine: 'underline',
-  },
-  padTop20: { paddingTop: 20},
+  subtitle: { fontSize: 20, color: "#38434D" },
+  padTop20: { paddingTop: 20 },
   colorWhite: { color: '#fff' },
   flex: { display: 'flex', flexDirection: 'row', justifyContent: 'center' },
+  button: {
+    backgroundColor: '#747480',
+    padding: 10,
+    alignItems: 'center',
+    alignSelf: 'center',
+    borderRadius: 15,
+    marginTop: 80
+  },
 });
